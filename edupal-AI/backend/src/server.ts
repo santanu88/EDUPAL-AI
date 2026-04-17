8d4ec4441a6909f851221014354940b84f64fcf5
Santanu Swain <shantanu.swain23@gmail.com>
2026-04-17 19:43:20 +0530

    update the server

--- edupal-AI/backend/src/server.ts
+++ edupal-AI/backend/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://edupal-ai.vercel.app/"],
+    origin: ["https://edupal-ai.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, 
   })
