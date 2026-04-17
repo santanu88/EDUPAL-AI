22672f9a3d35889e5b3a3b86921526eb1e570eb8
Santanu Swain <shantanu.swain23@gmail.com>
2026-04-17 20:24:01 +0530

    updated the server

--- edupal-AI/backend/src/server.ts
+++ edupal-AI/backend/src/server.ts
@@ -2,27 +2,30 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/ai-expert-routes";
+
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(
   cors({
-       origin: [
+    origin: [
       "https://edupal-ai.vercel.app",
       "https://edupal-ai-git-main-santanu-portfolio.vercel.app",
       "https://edupal-9aaegx5gx-santanu-portfolio.vercel.app",
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://localhost:5174",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
-const aiRouter = router;
-app.use("/ai", aiRouter);
+app.options("*", cors());
+app.use(express.json());
+app.use("/ai", router)
 
-const port = 5000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 5000;
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
